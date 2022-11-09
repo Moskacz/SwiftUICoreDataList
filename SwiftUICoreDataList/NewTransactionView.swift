754db2df74333b4934e4ae0b51824a7c67231946
Michał Moskała <moskala.michal@gmail.com>
2022-11-09 21:41:58 +0100

    Add view model

--- SwiftUICoreDataList/NewTransactionView.swift
+++ SwiftUICoreDataList/NewTransactionView.swift
@@ -12,31 +12,30 @@ struct NewTransactionView: View {
     @Binding
     var sheetPresented: Bool
     
-    @State
-    private var title: String = ""
+    @StateObject var viewModel: NewTransactionViewModel
     
-    @State
-    private var category: String = ""
-    
-    @State
-    private var amount: String = ""
+    init(sheetPresented: Binding<Bool>, viewModel: NewTransactionViewModel = NewTransactionViewModel()) {
+        self._sheetPresented = sheetPresented
+        self._viewModel = .init(wrappedValue: viewModel)
+    }
     
     var body: some View {
         VStack {
             HStack {
                 Text("Title:")
-                TextField("Tap..", text: $title)
+                TextField("Tap..", text: $viewModel.title)
             }
             HStack {
                 Text("Category:")
-                TextField("Tap..", text: $category)
+                TextField("Tap..", text: $viewModel.category)
             }
             HStack {
                 Text("Amount")
-                TextField("Tap..", text: $amount)
+                TextField("Tap..", text: $viewModel.amount)
             }
             Button(action: {
                 sheetPresented = false
+                viewModel.save()
             }, label: {
                 Text("Save")
             })
@@ -45,9 +44,25 @@ struct NewTransactionView: View {
             .background(Color.blue)
             .cornerRadius(8)
         }
+        .padding(EdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5))
         .lineSpacing(10)
         .background(Color.teal)
     }
 }
 
 
+final class NewTransactionViewModel: ObservableObject {
+    @Published var title: String = ":)"
+    @Published var category: String = ""
+    @Published var amount: String = ""
+    
+    func save() {
+        print([title, category, amount].joined(separator: " "))
+    }
+}
+
+struct NewTransactionView_Preview: PreviewProvider {
+    static var previews: some View {
+        NewTransactionView(sheetPresented: .constant(true), viewModel: NewTransactionViewModel())
+    }
+}
